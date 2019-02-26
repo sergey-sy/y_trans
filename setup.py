86b3fb6a9cbb97cf83eda5bddacf280d7cc3640e
sergey-sy <maiyashik@gmail.com>
2019-02-27 02:24:44 +0300

    First publication v0.0.1c

--- setup.py
+++ setup.py
@@ -5,15 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="y_trans",
-    version="0.0.1b",
+    version="0.0.1c",
     author="sergey-sy",
     author_email="maiyashik@gmail.com",
     description="Machine translation with Yandex.Translate API",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/sergey-sy/y_trans",
-    download_url="https://github.com/sergey-sy/y_trans/archive/v0.0.1b.tar.gz",
-    keywords=["yandex", "yandex translate", "yandex translator", "translator"],
     scripts=["bin/y_trans"],
     packages=setuptools.find_packages(),
     classifiers=[
